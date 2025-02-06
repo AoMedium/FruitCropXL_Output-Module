0cec72ecaae79b5be42b3968b06fccd77d876157
AoMedium <anoddmedium@gmail.com>
2025-02-07 11:32:04 +1300

    add support for writing array and lists + fix: allowing chaining after write()

--- src/test/java/io/github/fruitcropxl/output/util/StepwiseCsvWriterTest.java
+++ src/test/java/io/github/fruitcropxl/output/util/StepwiseCsvWriterTest.java
@@ -1,5 +1,8 @@
 package io.github.fruitcropxl.output.util;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.Test;
 
 import com.fasterxml.jackson.annotation.PropertyAccessor;
@@ -54,5 +57,53 @@ public class StepwiseCsvWriterTest {
         for (int i = 0; i < 5; i++) {
             writer.write(new Fruit(i, i * i));
         }
+    }
+
+    @Test
+    public void testWriteList() {
+        List<Fruit> list = new ArrayList<>();
+
+        for (int i = 0; i < 5; i++) {
+            list.add(new Fruit(i, i * i));
+        }
+
+        CsvMapper mapper = new CsvMapper();
+        mapper.setVisibility(PropertyAccessor.ALL, Visibility.NONE);
+        mapper.setVisibility(PropertyAccessor.FIELD, Visibility.DEFAULT);
+
+        String filePath = "target/write-list.csv";
+
+        (new StepwiseCsvWriter(filePath, mapper)).deleteFileIf(true).writeList(list);
+    }
+
+    @Test
+    public void testWriteArray() {
+        Fruit[] list = {
+                new Fruit(0, 0),
+                new Fruit(1, 0),
+                new Fruit(2, 0),
+                new Fruit(3, 0)
+        };
+
+        CsvMapper mapper = new CsvMapper();
+        mapper.setVisibility(PropertyAccessor.ALL, Visibility.NONE);
+        mapper.setVisibility(PropertyAccessor.FIELD, Visibility.DEFAULT);
+
+        String filePath = "target/write-array.csv";
+
+        (new StepwiseCsvWriter(filePath, mapper)).deleteFileIf(true).writeArray(list);
+    }
+
+    @Test
+    public void testWriteEmptyArray() {
+        Fruit[] list = {};
+
+        CsvMapper mapper = new CsvMapper();
+        mapper.setVisibility(PropertyAccessor.ALL, Visibility.NONE);
+        mapper.setVisibility(PropertyAccessor.FIELD, Visibility.DEFAULT);
+
+        String filePath = "target/write-empty-array.csv";
+
+        (new StepwiseCsvWriter(filePath, mapper)).deleteFileIf(true).writeArray(list);
     }
 }
